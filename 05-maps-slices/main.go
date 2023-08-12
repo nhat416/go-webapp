52a608976eac80b17f25f3fe726600dac7fe8ef3
Nhat Tran <nhatt416@gmail.com>
2023-08-11 21:43:22 -0400

    .

--- 05-maps-slices/main.go
+++ 05-maps-slices/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sort"
+)
 
 type User struct {
 	FirstName string
@@ -8,6 +11,7 @@ type User struct {
 }
 
 func main() {
+	/* maps
 	myMap := make(map[string]User)
 
 	me := User{
@@ -21,4 +25,34 @@ func main() {
 
 	//var myNewVar float32
 	//myNewVar = 11.1
+	*/
+
+	var myString string
+	myString = "Fish"
+
+	log.Println(myString)
+
+	var mySlice []string
+	var mySliceTwo []int
+
+	mySlice = append(mySlice, "Toronto")
+	mySlice = append(mySlice, "Maple")
+	mySlice = append(mySlice, "Leafs")
+
+	mySliceTwo = append(mySliceTwo, 2)
+	mySliceTwo = append(mySliceTwo, 1)
+	mySliceTwo = append(mySliceTwo, 3)
+
+	log.Println(mySlice)
+	log.Println(mySliceTwo)
+
+	sort.Ints(mySliceTwo)
+	log.Println(mySliceTwo)
+
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	log.Println(numbers)
+	log.Println(numbers[0:2])
+
+	names := []string{"Tavares", "Matthews", "Marner", "Nylander", "Rielly"}
+	log.Println(names)
 }
